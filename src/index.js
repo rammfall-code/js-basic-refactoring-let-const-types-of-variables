ee30f3a7ad886fee2046ad4df14897518a202d73
Anya <81263768+anyabuu@users.noreply.github.com>
2025-03-14 20:49:22 +0200

    errors fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+function getNumber(message) {
+  let number;
+
+  do {
+    number = prompt(message);
+
+    if (number === null) {
+      break;
+    }
+  } while (number === '' || isNaN(number));
+
+  return +number;
+}
+
 let sum = 0;
 
-let firstNumber = prompt('Enter first number');
+const firstNumber = getNumber('Enter first number');
 
-sum += +firstNumber;
+sum += firstNumber;
 
-let secondNumber = prompt('Enter second number');
+const secondNumber = getNumber('Enter second number');
 
-sum += +secondNumber;
+sum += secondNumber;
 
 alert(`Your sum is ${sum}`);
